c0c7b1d497eff635911932e8feba973f3ae23324
Shuning <ya.aif2002@yandex.ru>
2023-04-26 00:05:51 +0300

    Обновление handlers, main (добавление роутера) Редактирование инструкций в Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,10 @@ ENV PYTHONUNBUFFERED 1
 
 COPY requirements.txt /app/
 
-RUN pip install -r ./app/requirements.txt
+RUN pip install --no-cache-dir -r /app/requirements.txt
 
 COPY . /app
 
 WORKDIR /app
 
-CMD ["python uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["uvicorn", "app/main:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/handlers.py
+++ app/handlers.py
@@ -1,37 +1,36 @@
-from fastapi import FastAPI
-from app.main import app
+from fastapi import APIRouter
 
+router = APIRouter()
 
-
-@app.get('/api/docs')
+@router.get('/api/docs')
 def read_docs():
     return {
         'message': 'API documentation'
     }
 
 
-@app.post('/api/{process_name}', name='start')
+@router.post('/api/{process_name}', name='start')
 def start_proc(process_name: str):
     return {
         'message': 'process started'
     }
 
 
-@app.post('/api/{process_name}', name='stop')
+@router.post('/api/{process_name}', name='stop')
 def stop_proc(process_name: str):
     return {
         'message': 'process stopped'
     }
 
 
-@app.get('/api/{process_name}')
+@router.get('/api/{process_name}')
 def get_status(process_name: str):
     return {
         'status': 'running'
     }
 
 
-@app.get('/api/{process_name}/result')
+@router.get('/api/{process_name}/result')
 def get_result(process_name: str):
     return {
         'result': 'some result'
